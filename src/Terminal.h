26f8a31e2f472473a999c8bc46a100f8463c5e7d
akash1474 <panditakash38@gmail.com>
2024-10-18 22:48:19 +0530

    Terminal Input Handling + Unicode support

--- src/Terminal.h
+++ src/Terminal.h
@@ -1,3 +1,6 @@
+#include "Coordinates.h"
+#include "DataTypes.h"
+#include "imgui_internal.h"
 #include "string"
 #include "vector"
 #include "mutex"
@@ -19,13 +22,12 @@ private:
     std::mutex mOutputMutex;          // To synchronize output access
     bool mIsCommandRunning;           // Flag to indicate a command is running
     bool mScrollToBottom;             // Flag to auto-scroll output
-    std::string mBuffer,mDisplayBuffer;
+    std::string mBuffer;
     std::thread mThread;
     bool mTerminalThread;
     ConPTY mConPTY;
-    size_t mPrevBufferSize=0;
     double mPrevTime=0;
-    // Shell process variables
+    
 
 
     void StartShell();
@@ -34,4 +36,53 @@ private:
 
     // Function to send a command to the shell
     void RunCommand(std::string& command);
+
+
+private:
+    //Command History
+    std::vector<std::string> mHistory;
+    size_t mHistoryIdx{0};
+    void PushHistory(const std::string& cmd);
+    void PushHistoryCommand(bool direction);
+
+
+private:
+
+    enum class SelectionMode { Normal, Word, Line };
+    SelectionMode mSelectionMode{SelectionMode::Normal};
+
+    // Text Renderer
+    EditorState mState;
+    std::vector<std::string> mLines;
+    ImGuiWindow* mWindow=nullptr;
+    ImVec2 mPosition;
+    ImVec2 mCharacterSize;
+    ImVec2 mLinePosition;
+    ImRect mBounds;
+    ImVec2 mSize;
+    float mLastClick;
+    float mMinLineVisible;
+    float mLineSpacing{5.0f};
+    int mLineHeight{0};
+    float mTitleBarHeight{0};
+    uint8_t mTabWidth{4};
+    bool mRecalculateBounds=false;
+    Coordinates mReadOnlyCoords;
+    std::string mCommand;
+
+    float mPaddingLeft = 5.0f;
+
+    void Draw();
+    void UpdateBounds();
+    float GetSelectionPosFromCoords(const Coordinates& coords)const;
+    size_t GetLineMaxColumn(int currLineIndex)const;
+    void AppendText(const std::string& str);
+    Coordinates GetCommandInsertPosition();
+    int GetCharacterIndex(const Coordinates& aCoordinates) const;
+    std::pair<int,int> GetIndexOfWordAtCursor(const Coordinates& coords)const;
+    Coordinates MapScreenPosToCoordinates(const ImVec2& mousePosition);
+    void HandleInputs();
+    void ExitSelectionMode(){mSelectionMode=SelectionMode::Normal;};
+    int GetCharacterColumn(int aLine, int aIndex) const;
+
 };
